b3fa6058aa467c7a421674a086099fabb8de063b
badAl <badalsingh120403@gmail.com>
2025-01-22 15:05:32 +0530

    feat: generate certificate and key using openssl as docs says

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { createServer } from "node:https";
+import fs from "node:fs";
 
 const PORT = 9000;
 
@@ -17,4 +19,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT);
+/**
+ * Genereate  the certificate and key {COPIED FROM NODE HTTPS DOCS}
+ *
+ * openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' \
+  -keyout private-key.pem -out certificate.pem
+ */
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
+};
+
+createServer(options, app).listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+});
