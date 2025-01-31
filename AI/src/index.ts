5e8a4b17cf3da99dedcc5b29571fd64f43341976
badAl <badalsingh120403@gmail.com>
2025-01-31 18:55:14 +0530

    feat: chunk text use textSplitter splitDocument
    - seperate cunk from endline
    - ser chunksize to 200 for now
    -

--- AI/src/index.ts
+++ AI/src/index.ts
@@ -3,19 +3,33 @@ import 'dotenv/config'
 import chalk from 'chalk'
 import { TextLoader } from 'langchain/document_loaders/fs/text'
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
+import {
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+} from '@langchain/textsplitters'
 
 const geminModel = new ChatGoogleGenerativeAI({
     apiKey: process.env.GEMINI_API_KEY,
 })
 
+const textSplitter = new CharacterTextSplitter({
+    separator: '\n',
+    chunkSize: 200,
+    chunkOverlap: 0,
+})
+
 const loader = new TextLoader('./data/facts.txt')
 
 const loadDocs = async () => {
-    const docs = await loader.load()
-    console.log(chalk.blue('DOCS'))
-    console.log(docs[0])
-    console.log(chalk.yellow('Metadata'))
-    console.log(docs[0].metadata)
+    const rawDocuments = await loader.load()
+    const docs = textSplitter.splitDocuments(rawDocuments)
+
+    console.log(
+        (await docs).forEach((doc) => {
+            console.log(chalk.cyan('-').repeat(81))
+            console.log(doc.pageContent)
+        }),
+    )
 }
 
 loadDocs()
