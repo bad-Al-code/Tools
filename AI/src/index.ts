deb8b62c1d8b025b2627b3c87103f764e71e25f3
badAl <badalsingh120403@gmail.com>
2025-01-31 19:44:19 +0530

    feat: use chroma as a vector space for storing embedding

--- AI/src/index.ts
+++ AI/src/index.ts
@@ -3,12 +3,15 @@ import 'dotenv/config'
 import chalk from 'chalk'
 import { TextLoader } from 'langchain/document_loaders/fs/text'
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
-import {
-    CharacterTextSplitter,
-    RecursiveCharacterTextSplitter,
-} from '@langchain/textsplitters'
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai'
+import { CharacterTextSplitter } from '@langchain/textsplitters'
+import { Chroma } from '@langchain/community/vectorstores/chroma'
 
 const geminModel = new ChatGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY,
+})
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
     apiKey: process.env.GEMINI_API_KEY,
 })
 
@@ -22,14 +25,21 @@ const loader = new TextLoader('./data/facts.txt')
 
 const loadDocs = async () => {
     const rawDocuments = await loader.load()
-    const docs = textSplitter.splitDocuments(rawDocuments)
+    const docs = await textSplitter.splitDocuments(rawDocuments)
 
-    console.log(
-        (await docs).forEach((doc) => {
-            console.log(chalk.cyan('-').repeat(81))
-            console.log(doc.pageContent)
-        }),
+    const db = await Chroma.fromDocuments(docs, embeddings, {
+        collectionName: 'facts-collection',
+    })
+
+    const results = await db.similaritySearch(
+        'What is an intersting fact about the english lanugage?',
     )
+
+    for (let result of results) {
+        console.log(chalk.cyan('-').repeat(81))
+        // console.log(result[1])
+        console.log(result.pageContent)
+    }
 }
 
 loadDocs()
