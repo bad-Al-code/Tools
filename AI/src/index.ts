b2faa45f8663e8c8b63506fda7d5d7c67814bf47
badAl <badalsingh120403@gmail.com>
2025-01-31 18:27:18 +0530

    feat: load txt file using langchain textLoader class

--- AI/src/index.ts
+++ AI/src/index.ts
@@ -1,41 +1,21 @@
 import 'dotenv/config'
 
-import { OpenAI } from '@langchain/openai'
 import chalk from 'chalk'
-import inquirer from 'inquirer'
+import { TextLoader } from 'langchain/document_loaders/fs/text'
+import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 
-const openai = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY })
+const geminModel = new ChatGoogleGenerativeAI({
+    apiKey: process.env.GEMINI_API_KEY,
+})
 
-async function generatecode(task: string, language: string) {
-    const prompt = `Write a program in ${language} to accomplish the following task: ${task}. Provide only the code without explanations.`
+const loader = new TextLoader('./data/facts.txt')
 
-    try {
-        console.log(chalk.blue('\nGenerating Code '))
-        const response = await openai.invoke(prompt)
-        console.log(chalk.green('Generated Code \n'))
-        console.log(chalk.yellow(response))
-    } catch (err) {
-        console.error(chalk.red('Error generating code ', err))
-    }
+const loadDocs = async () => {
+    const docs = await loader.load()
+    console.log(chalk.blue('DOCS'))
+    console.log(docs[0])
+    console.log(chalk.yellow('Metadata'))
+    console.log(docs[0].metadata)
 }
 
-async function main() {
-    console.log(chalk.cyan('\n Langchain Code Genreator CLI \n'))
-
-    const answers = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'task',
-            message: 'Enter the task for which you wnat code: ',
-        },
-        {
-            type: 'input',
-            name: 'language',
-            message: 'Enter the programming language',
-        },
-    ])
-
-    await generatecode(answers.task, answers.language)
-}
-
-main()
+loadDocs()
