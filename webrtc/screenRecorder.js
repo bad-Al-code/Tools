87c43212d3ece9ab55a73d58fe8eeefc67302a67
Badal <badalsingh120403@gmail.com>
2025-02-19 19:24:24 +0530

    style: add button color depending on their state in scren recorder

--- webrtc/screenRecorder.js
+++ webrtc/screenRecorder.js
@@ -2,6 +2,10 @@ let mediaRecorder;
 let recorededBlobs;
 
 const startRecording = () => {
+    if (!stream) {
+        alert('No current feed');
+        return;
+    }
     recorededBlobs = [];
     mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.ondataavailable = (e) => {
@@ -10,16 +14,57 @@ const startRecording = () => {
     };
 
     mediaRecorder.start();
+
+    changeButtons([
+        'green',
+        'green',
+        'blue',
+        'blue',
+        'green',
+        'blue',
+        'grey',
+        'blue',
+    ]);
 };
 
 const stopRecording = () => {
+    if (!mediaRecorder) {
+        alert('Please record before stopping');
+        return;
+    }
     mediaRecorder.stop();
+
+    changeButtons([
+        'green',
+        'green',
+        'blue',
+        'blue',
+        'green',
+        'green',
+        'blue',
+        'blue',
+    ]);
 };
 
 const playRecording = () => {
+    if (!recorededBlobs) {
+        alert('No recording found');
+        return;
+    }
     const superBuffer = new Blob(recorededBlobs);
     const recorededVideoEl = document.querySelector('#other-video');
     recorededVideoEl.src = window.URL.createObjectURL(superBuffer);
     recorededVideoEl.controls = true;
     recorededVideoEl.play();
+
+    changeButtons([
+        'green',
+        'green',
+        'blue',
+        'blue',
+        'green',
+        'green',
+        'green',
+        'blue',
+    ]);
 };
