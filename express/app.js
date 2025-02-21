609b10b78bdabf2fb33201a0719e0c866e64d053
Badal <badalsingh120403@gmail.com>
2025-02-21 13:50:42 +0530

    feat: express middleware

--- express/app.js
+++ express/app.js
@@ -1,14 +1,26 @@
-import express from 'express';
+﻿import express, { json, urlencoded } from 'express';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import helmet from 'helmet';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.all('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    }),
+);
+
+app.use(express.static('public'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+
+app.post('/', (req, res) => {
+    console.log(req.headers);
+    res.send('Test');
 });
 
 export { app };
