1eca3ba18b01659b8ac0c42e2e2b3d964366a8ef
Badal <badalsingh120403@gmail.com>
2025-02-21 13:27:13 +0530

    feat: serve static file using express

--- express/app.js
+++ express/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.all('*', (req, res) => {
-    res.send('Hello');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 export { app };
