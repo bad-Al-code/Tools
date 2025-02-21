57004a080524977a425ef1eea1f4608420f35c2c
Badal <badalsingh120403@gmail.com>
2025-02-21 14:26:24 +0530

    server side rendering using pug view engine

--- express/app.js
+++ express/app.js
@@ -1,10 +1,5 @@
 ﻿import express, { json, urlencoded } from 'express';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
 import helmet from 'helmet';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -16,11 +11,13 @@ app.use(
 
 app.use(express.static('public'));
 app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
 
 app.post('/', (req, res) => {
     console.log(req.headers);
-    res.send('Test');
+    res.json('Test');
 });
 
 export { app };
