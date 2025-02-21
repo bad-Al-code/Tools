dbd1011f1a3ede831cd0e960514f0fcbcb133fed
Badal <badalsingh120403@gmail.com>
2025-02-22 05:26:15 +0530

    feat(webrtc): add ice server configuration and peer connectopn setup
    - added stun servers tp `peerConfiguration` for improved nat traversal
    - implemented `createPeerConnection` to establish a webrtc connection
    - updated `call` function to initialize peer connection and create an
    offer
    - add event listener for ice candidate discovery

--- webrtc/signalingPeerConnection/scripts.js
+++ webrtc/signalingPeerConnection/scripts.js
@@ -5,12 +5,50 @@ let localStream;
 let remoteStream;
 let peerConnection;
 
+let peerConfiguration = {
+    iceServers: [
+        {
+            urls: [
+                'stun:stun.l.google.com:19302',
+                'stun:stun1.l.google.com:19302',
+            ],
+        },
+    ],
+};
+
 const call = async (e) => {
     const stream = await navigator.mediaDevices.getUserMedia({
         video: true,
     });
 
     localVideoEl.srcObject = stream;
+    localStream = stream;
+
+    await createPeerConnection();
+
+    try {
+        console.log('Creating offer..');
+        const offer = await peerConnection.createOffer();
+        console.log(offer);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const createPeerConnection = () => {
+    return new Promise(async (resolve, reject) => {
+        peerConnection = await new RTCPeerConnection(peerConfiguration);
+
+        localStream.getTracks().forEach((track) => {
+            peerConnection.addTrack(track, localStream);
+        });
+
+        peerConnection.addEventListener('icecandidate', (e) => {
+            console.log('.... ICE CANDIDATE FOUND....');
+            console.log(e);
+        });
+        resolve();
+    });
 };
 
 document.querySelector('#call').addEventListener('click', call);
