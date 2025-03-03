efa612ce233ae35f3441bf41e9d67df92b384476
Badal <badalsingh120403@gmail.com>
2025-03-03 13:02:28 +0530

    feat(transcriber): initial setup

---
+++ aws/transcribe/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const app = express();
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
