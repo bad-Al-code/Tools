c4a86b8db9189662be7fdaed2385b36e4704462c
Badal <badalsingh120403@gmail.com>
2025-03-12 00:23:54 +0530

    feat(express): creat login and register template

--- express/src/index.ts
+++ express/src/index.ts
@@ -15,23 +15,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', async (req: Request, res: Response) => {
-    try {
-        const [rows] = await pool.query('SELECT * FROM dashboard_data');
-        res.render('index', { data: rows });
-    } catch (error) {
-        console.error('Database error:', error);
-        res.status(500).send('Internal Server Error');
-    }
+app.get('/', (req: Request, res: Response) => {
+    res.render('index');
 });
 
-app.get('/about', (req: Request, res: Response) => {
-    res.render('about');
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
-app.get('/contact', (req: Request, res: Response) => {
-    res.render('contact');
+app.get('/register', (req, res) => {
+    res.render('register');
 });
+
+app.post('/register', (req, res) => {});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
