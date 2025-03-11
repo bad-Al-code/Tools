765a720daf35132ef6668f3ac5dee1ac8e422f76
Badal <badalsingh120403@gmail.com>
2025-03-11 23:39:26 +0530

    chore(express): use helemt middleware

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,8 +1,11 @@
 import path from 'node:path';
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(helmet());
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
